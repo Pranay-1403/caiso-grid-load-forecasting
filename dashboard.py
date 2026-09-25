34757b4da87e7d668ce208d68d75f518d59dd790
Sai Pranay Mosarapu <pranay232026@gmail.com>
2026-09-25 11:40:16 +0530

    Update API endpoint for forecast request

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,7 @@ st.write("This dashboard connects directly to your active FastAPI microservice."
 
 if st.button("Get 24-Hour Forecast"):
     # Send a request to the FastAPI backend running on local port 8000
-    response = requests.get("http://127.0.0.1:8000/predict")
+    response = requests.get("https://caiso-grid-load-forecasting.onrender.com/predict")
 
     if response.status_code == 200:
         st.success("Successful response from FastAPI!")
